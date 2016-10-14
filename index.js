abdaed45dfd9a12ba75bbf5936d6743b67348f68
Seven Yu <dofyyu@gmail.com>
2016-10-14 19:05:17 +0800

    add missing 2 class contents, need review

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
 var app = require('express')(),
     bodyParser = require ('body-parser');
+var wechat = require('./lib/wechat');
+var config = require('./lib/config');
 
 app.use(bodyParser.urlencoded({
     extended: false
 }));
+
+wechat(config.wechat);
+
+wechat.createMenu(require('./lib/menu.json'));
 
 app.use('/wxapi', require('./lib/routers/wxapi.js'));
 
